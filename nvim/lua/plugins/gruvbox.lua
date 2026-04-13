75a71f548a516b51873893523f8ca35a9a3023f9
Abdul-hameed <abdulhameedadedimeji@gmail.com>
2026-04-13 15:23:40 +0100

    fix: Fixed theme not being applied, LOL

--- nvim/lua/plugins/gruvbox.lua
+++ nvim/lua/plugins/gruvbox.lua
@@ -1,17 +1,20 @@
--- Thank you Tony!
-local function enable_transparency()
-  vim.cmd "hi Directory guibg=NONE"
-  vim.cmd "hi SignColumn guibg=NONE"
-  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-  -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-  -- vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
-end
+-- local function enable_transparency()
+--   vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+--   vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+--   vim.api.nvim_set_hl(0, "Directory", { bg = "none" })
+-- end
 
 return {
   "ellisonleao/gruvbox.nvim",
   priority = 1000,
   config = function()
-    vim.cmd.colorscheme = "gruvbox"
-    enable_transparency()
+    require("gruvbox").setup {
+      contrast = "soft",
+      transparent_mode = true,
+    }
+
+    vim.cmd.colorscheme "gruvbox"
+
+    -- enable_transparency()
   end,
 }
