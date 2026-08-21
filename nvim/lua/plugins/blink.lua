93754ce87da8bd1bde913c70d32d39a33288e8cb
Abdul-hameed <abdulhameedadedimeji@gmail.com>
2026-08-21 23:01:52 +0100

    chore: change autocomplete from Tab to Enter

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -13,7 +13,7 @@ return {
     -- 'super-tab' for mappings similar to vscode (tab to accept)
     -- 'enter' for enter to accept
     -- 'none' for no mappings
-    keymap = { preset = "super-tab" },
+    keymap = { preset = "enter" },
 
     appearance = {
       nerd_font_variant = "mono",
