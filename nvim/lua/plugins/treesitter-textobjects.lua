6a653f92dfe2c5e0989ead9704f1234f599d7e4b
Abdul-hameed <abdulhameedadedimeji@gmail.com>
2026-08-23 16:48:11 +0100

    chore: refactoring...

--- nvim/lua/plugins/treesitter-textobjects.lua
+++ nvim/lua/plugins/treesitter-textobjects.lua
@@ -57,13 +57,13 @@ return {
     end)
 
     -- function
-    vim.keymap.set({ "n", "x", "o" }, "]m", function()
+    vim.keymap.set({ "n", "x", "o" }, "]f", function()
       require("nvim-treesitter-textobjects.move").goto_next_start("@function.inner", "textobjects")
-    end)
+    end, { noremap = true })
 
-    vim.keymap.set({ "n", "x", "o" }, "[m", function()
+    vim.keymap.set({ "n", "x", "o" }, "[f", function()
       require("nvim-treesitter-textobjects.move").goto_previous_start("@function.inner", "textobjects")
-    end)
+    end, { noremap = true })
 
     vim.keymap.set({ "n", "x", "o" }, "]M", function()
       require("nvim-treesitter-textobjects.move").goto_next_start("@function.outer", "textobjects")
@@ -96,5 +96,10 @@ return {
     -- ensure ; goes forward and , goes backward regardless of the last direction
     vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next)
     vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous)
+
+    vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f_expr, { expr = true })
+    vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F_expr, { expr = true })
+    vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t_expr, { expr = true })
+    vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T_expr, { expr = true })
   end,
 }
